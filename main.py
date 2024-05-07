722e0878785a3ffbe48d97025c64d607ccb35b47
Harsh Chauhan <chauhanharsh643@gmail.com>
2024-05-07 17:36:16 +0530

    Displaying the output in following forrmat Lat: x.ysas and Long: z.ysas.

--- main.py
+++ main.py
@@ -6,5 +6,16 @@ response = urllib.request.urlopen(req)
 
 obj = json.loads(response.read().decode('utf-8'))
 
+def format_location(latitude, longitude, decimal_places=4):
+    lat_str = "{:.{}f}".format(latitude, decimal_places)
+    long_str = "{:.{}f}".format(longitude, decimal_places)
+    return "Lat: {} and Long: {}".format(lat_str, long_str)
+
 print(obj['timestamp'])
-print(obj['iss_position']['latitude'], obj['iss_position']['longitude'])
+
+# storing lat and log inside a variable
+latitude = float(obj['iss_position']['latitude'])
+longitude = float(obj['iss_position']['longitude'])
+formatted_location = format_location(latitude, longitude)
+print(formatted_location)
+
